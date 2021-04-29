5a71df7901512988d203ad546f2276d4f1431a1d
22yuu <22yuu@naver.com>
2021-04-29 16:07:37 +0900

    2021/04/29 ch05 내장함수,스코프,리스트 예제 추가

---
+++ ch05/5_4_FunctionBuiltin.py
@@ -0,0 +1,22 @@
+'''
+날짜 : 2021/04/28
+이름 : 이진유
+내용 : 파이썬 내장함수 실습하기 교재 p118
+'''
+
+import time
+
+r1 = abs(-5)
+print('r1:', r1)
+
+
+
+now = time.localtime(time.time())
+year = time.strftime('%Y', now)
+month = time.strftime('%m', now)
+date = time.strftime('%d', now)
+hour = time.strftime('%H', now)
+min = time.strftime('%M', now)
+sec = time.strftime('%s', now)
+
+print('%s년 %s월 %s일')
